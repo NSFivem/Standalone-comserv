921d0d78e899eabb721f03ec17c80f22a7c621e6
NS100 <86939732+NSFivem@users.noreply.github.com>
2021-12-22 20:04:27 -0500

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,24 @@
+fx_version 'cerulean'
+games { 'gta5' }
+
+-- discord.gg/nsdev --
+-- discord.gg/nsdev --
+
+author'NS100#0001' -- just made it standalone and made changes to the script, I didnt make this whole script from scratch
+
+-- discord.gg/nsdev --
+-- discord.gg/nsdev --
+
+
+
+client_scripts {
+  '@mysql-async/lib/MySQL.lua',
+  'client/zaza_comserv.lua',
+}
+
+server_scripts {
+  '@mysql-async/lib/MySQL.lua',
+  'server/sv_comserv.lua'
+}
+
+
